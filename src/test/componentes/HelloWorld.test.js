b07d4f23afd6c230da3579b68af88c8f7693a967
rinel <rineliniguezsosa@gmail.com>
2023-04-10 17:07:44 -0600

    esperamos a que la palabra HelloWorld deberia de estar en el documento

--- src/test/componentes/HelloWorld.test.js
+++ src/test/componentes/HelloWorld.test.js
@@ -5,5 +5,7 @@ describe('Pruebas en con componente HelloWorld.js', () => {
     test('El componente HelloWorld debe de renderizarse correctamente', () => {
         render(<HelloWorld/>)
         const Hello = screen.getByText('HelloWorld')
+
+        expect(Hello).toBeInTheDocument()
     });
 });
