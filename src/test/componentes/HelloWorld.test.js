000957537b834b553e0feb22a43c96f11aa316b1
rinel <rineliniguezsosa@gmail.com>
2023-04-10 16:54:50 -0600

    importando el componente HelloWorld

--- src/test/componentes/HelloWorld.test.js
+++ src/test/componentes/HelloWorld.test.js
@@ -1,4 +1,5 @@
 import { render,screen } from '@testing-library/react'
+import { HelloWorld } from '../../componentes/HelloWorld';
 
 describe('Pruebas en con componente HelloWorld.js', () => {
     
