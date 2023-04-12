e39c8125169ba8bb35a08b305972c7a25a2d3986
rinel <rineliniguezsosa@gmail.com>
2023-04-12 14:48:37 -0600

    modificando la prueba "El componente HelloWorld debe de renderizarse la palabra Hello"

--- src/test/componentes/HelloWorld.test.js
+++ src/test/componentes/HelloWorld.test.js
@@ -1,15 +1,15 @@
-import { render,screen } from '@testing-library/react'
+import {render,screen} from '@testing-library/react'
 import { HelloWorld } from '../../componentes/HelloWorld';
 
 describe('Pruebas en con componente HelloWorld.js', () => {
-    test('El componente HelloWorld debe de renderizarse correctamente', () => {
+
+    test('El componente HelloWorld debe de renderizarse la palabra Hello', () => {
+        
         render(<HelloWorld/>)
-        const Hello = screen.getByText('Hello World')
 
-        expect(Hello).toBeInTheDocument()
-    });
+        const HelloText = screen.getByText('Hello')
 
-    test('El componente HelloWorld debe de renderizarse pasando la prop name', () => {
-        render(<HelloWorld name="Rinel"/>)
+        expect(HelloText).toBeTruthy();
+        
     });
 });
