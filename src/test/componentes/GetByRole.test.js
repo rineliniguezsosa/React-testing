4d1f24254405b72c0e9c7ec1903d54ec1e788691
rinel <rineliniguezsosa@gmail.com>
2023-04-12 20:17:54 -0600

    renderizando el componente GetByRole

--- src/test/componentes/GetByRole.test.js
+++ src/test/componentes/GetByRole.test.js
@@ -1,8 +1,11 @@
+import { render,screen } from '@testing-library/react';
 import { GetByRole } from "../../componentes/GetByRole";
-import { render,screen } from '@testing-library/react'
+
 
 describe('pruebas en GetByRole.js', () => {
+
     test('debe de renderizar correctamente el componente', () => {
-        
+
+        render(<GetByRole/>)
     });
 });
