559f3921f597fde6534f006d593c563562a2afc1
rinel <rineliniguezsosa@gmail.com>
2023-04-12 19:16:06 -0600

    creando el bloque test con la sig descripcion "debe de renderizar correctamente el componente"

--- src/test/componentes/GetByRole.test.js
+++ src/test/componentes/GetByRole.test.js
@@ -2,5 +2,7 @@ import { GetByRole } from "../../componentes/GetByRole";
 
 
 describe('pruebas en GetByRole.js', () => {
-    
+    test('debe de renderizar correctamente el componente', () => {
+        
+    });
 });
