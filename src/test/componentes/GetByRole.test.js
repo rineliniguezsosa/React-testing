cd976448cc1d481f1185685a7bcc9d121f4a5971
rinel <rineliniguezsosa@gmail.com>
2023-04-13 19:53:28 -0600

    corrigiendo la imporacion del componente Aplication

--- src/test/componentes/GetByRole.test.js
+++ src/test/componentes/GetByRole.test.js
@@ -1,5 +1,5 @@
 import { render,screen } from '@testing-library/react';
-import { GetByRole } from "../../componentes/GetByRole";
+import { Aplication } from "../../componentes/Aplication";
 
 
 describe('pruebas en GetByRole.js', () => {
