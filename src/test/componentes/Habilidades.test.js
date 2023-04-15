5aad9472a4f6528509f3e420a165873482fceee4
rinel <rineliniguezsosa@gmail.com>
2023-04-14 21:07:42 -0600

    creando el bloque test con el siguiente titulo: debe de renderizar correctamente

--- src/test/componentes/Habilidades.test.js
+++ src/test/componentes/Habilidades.test.js
@@ -2,5 +2,9 @@ import { render,screen } from '@testing-library/react'
 import { Habilidades } from '../../componentes/Habilidades'
 
 describe('pruebas en el componente Habilidades.js', () => {
-    
+
+    test('debe de renderizar correctamente', () => {
+        
+    });
+
 });
