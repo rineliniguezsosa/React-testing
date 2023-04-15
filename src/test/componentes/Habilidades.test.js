e51615e30d671fbd201f65de8c044101caf7021b
rinel <rineliniguezsosa@gmail.com>
2023-04-14 21:08:44 -0600

    creando el array skills

--- src/test/componentes/Habilidades.test.js
+++ src/test/componentes/Habilidades.test.js
@@ -2,7 +2,7 @@ import { render,screen } from '@testing-library/react'
 import { Habilidades } from '../../componentes/Habilidades'
 
 describe('pruebas en el componente Habilidades.js', () => {
-
+    const skills = ['react','javascript','sql']
     test('debe de renderizar correctamente', () => {
         
     });
