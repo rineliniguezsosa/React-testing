714106d1d59aaf49cefc6746d977bd02ee8857ca
rinel <rineliniguezsosa@gmail.com>
2023-04-14 21:04:23 -0600

    creando el bloque describe con la siguiente titulo: pruebas en el componente Habilidades.js

--- src/test/componentes/Habilidades.test.js
+++ src/test/componentes/Habilidades.test.js
@@ -1,2 +1,6 @@
 import { render,screen } from '@testing-library/react'
 import { Habilidades } from '../../componentes/Habilidades'
+
+describe('pruebas en el componente Habilidades.js', () => {
+    
+});
