48694a643c75826dfac39cd322b7dd2247d046fb
rinel <rineliniguezsosa@gmail.com>
2023-04-14 21:23:10 -0600

    montando el componente Habilidades y pasando la prop skills

--- src/test/componentes/Habilidades.test.js
+++ src/test/componentes/Habilidades.test.js
@@ -4,6 +4,7 @@ import { Habilidades } from '../../componentes/Habilidades'
 describe('pruebas en el componente Habilidades.js', () => {
     const skills = ['react','javascript','sql']
     test('debe de renderizar correctamente', () => {
+        render(<Habilidades skills={skills}/>)
         
     });
 
