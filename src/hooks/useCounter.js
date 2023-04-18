efaa85bff65b2c684f32b1409ce756d3333d6227
rinel <rineliniguezsosa@gmail.com>
2023-04-18 17:58:10 -0600

    creamos la funcion increment donde aumentaremos el valor de count

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -3,6 +3,10 @@ import { useState } from 'react';
 
 export const useCounter = (cuentainicial = 0) => {
   const [count, setCount] = useState(cuentainicial)
+
+  const increment = () =>{
+    setCount(count+1)
+  }
   return {
 
   }
