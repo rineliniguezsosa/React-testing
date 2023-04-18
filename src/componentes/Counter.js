eba3f34cbce70e53c0f05b820444bf6db775025e
rinel <rineliniguezsosa@gmail.com>
2023-04-18 13:01:31 -0600

    agregando el elemento h1 con el valor de la cuenta y agregando el boton el cual actualiza el state

--- src/componentes/Counter.js
+++ src/componentes/Counter.js
@@ -4,6 +4,9 @@ import { useState } from 'react';
 export const Counter = () => {
   const [cuenta, setCuenta] = useState(0)
   return (
-    <div>Counter</div>
+    <div>
+        <h1>{cuenta}</h1>
+        <button onClick={()=> setCuenta(cuenta+1)}>incrementar</button>
+    </div>
   )
 }
