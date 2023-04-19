54ca58bb91a16add312db2d79c43ef63116f3787
rinel <rineliniguezsosa@gmail.com>
2023-04-18 18:11:26 -0600

    importando renderHook de testing-library

--- src/test/hooks/useCounter.test.js
+++ src/test/hooks/useCounter.test.js
@@ -1,4 +1,6 @@
+import { renderHook } from "@testing-library/react";
 import { useCounter } from "../../hooks/useCounter";
+
 
 describe('pruebas en useCounter hook', () => {
     test('debe de renderizar el valor inicial', () => {
