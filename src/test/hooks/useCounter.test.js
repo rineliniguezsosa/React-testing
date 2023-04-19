2edc3085755557d07709c0685f9d02547313482c
rinel <rineliniguezsosa@gmail.com>
2023-04-18 18:08:40 -0600

    creando el bloque test con el siguiente titulo:debe de renderizar el valor inicial

--- src/test/hooks/useCounter.test.js
+++ src/test/hooks/useCounter.test.js
@@ -1,5 +1,7 @@
 import { useCounter } from "../../hooks/useCounter";
 
 describe('pruebas en useCounter hook', () => {
-    
+    test('debe de renderizar el valor inicial', () => {
+        
+    });
 });
