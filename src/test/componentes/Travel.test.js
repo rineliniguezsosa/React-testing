5d7a0c18f54d363eecd5e9205914c604f9f6897c
rinel <rineliniguezsosa@gmail.com>
2023-04-19 15:45:36 -0600

    importando el componente Travel.js

--- src/test/componentes/Travel.test.js
+++ src/test/componentes/Travel.test.js
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react'
+import { Travel } from '../../componentes/Travel';
 
 describe('pruebas en Travel.js', () => {
     test('debe de coincidir con el snapshot', () => {
