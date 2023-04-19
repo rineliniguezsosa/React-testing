2a7be757fd8f3e7fff65a366d6ccf5fb763333a4
rinel <rineliniguezsosa@gmail.com>
2023-04-19 14:54:02 -0600

    pegando el contenido de Travel.js

--- src/componentes/Travel.js
+++ src/componentes/Travel.js
@@ -2,6 +2,15 @@
 
 export const Travel = () => {
   return (
-    <div>Travel</div>
+    <div className="card text-center m-1" style={{ width: '18rem' }}>
+      <i className="material-icons" style={{ fontSize: '4rem' }}>
+        airplanemode_active
+      </i>
+      <h4>Travel Anywhere</h4>
+      <p className="p-1">
+        Our premium package allows you to take exotic trips anywhere at the
+        cheapest prices!
+      </p>
+    </div>
   )
 }
