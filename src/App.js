2e3ffa2567b58b69c8ba4363c542161acceec3b8
rinel <rineliniguezsosa@gmail.com>
2023-04-19 14:55:06 -0600

    importando el componente Travel y comentando la linea 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // import { Aplication } from './componentes/Aplication';
 //import { Counter } from './componentes/Counter';
-import { JumboTron } from './componentes/JumboTron';
+//import { JumboTron } from './componentes/JumboTron';
+import { Travel } from './componentes/Travel';
+
 
 function App() {
   return (
