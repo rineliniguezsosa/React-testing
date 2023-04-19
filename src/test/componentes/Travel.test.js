afb90403b9df01959a4d27e3cef8307f840f057f
rinel <rineliniguezsosa@gmail.com>
2023-04-19 15:47:39 -0600

    desestructurando el container del componente  Travel

--- src/test/componentes/Travel.test.js
+++ src/test/componentes/Travel.test.js
@@ -3,6 +3,7 @@ import { Travel } from '../../componentes/Travel';
 
 describe('pruebas en Travel.js', () => {
     test('debe de coincidir con el snapshot', () => {
+        const { container } = render(<Travel></Travel>)
         
     });
 });
