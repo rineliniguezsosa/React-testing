7c1b6bd89e00d7917ebadabc7319be2701aae03a
rinel <rineliniguezsosa@gmail.com>
2023-04-19 16:04:07 -0600

    pegando contenido de Table.js

--- src/componentes/Table.js
+++ src/componentes/Table.js
@@ -1,7 +1,26 @@
 
 
-export const Table = () => {
+export const Table = ({employees}) => {
   return (
-    <div>Table</div>
+    <table className="table table-striped">
+      <thead className="thead-dark">
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Department</th>
+          <th scope="col">Title</th>
+        </tr>
+      </thead>
+      <tbody>
+        {employees.map(employee => {
+          return (
+            <tr key={employee.id}>
+              <td>{employee.name}</td>
+              <td>{employee.department}</td>
+              <td>{employee.title}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
   )
 }
