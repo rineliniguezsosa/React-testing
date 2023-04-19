4edb85131fd363dac55ab05deb12978027c396f0
rinel <rineliniguezsosa@gmail.com>
2023-04-19 16:01:29 -0600

    creando los objetos

--- src/data/fakeEmployees.js
+++ src/data/fakeEmployees.js
@@ -1,3 +1,14 @@
 export const fakeEmployees = [
-    
+    {
+        id:0,
+        name:'john smith',
+        department:'sales',
+        title:'senior sales agent'
+    },
+    {
+        id:1,
+        name:'sarah jenkins',
+        department:'engineering',
+        title:'senior full-stack agent'
+    }
 ]
