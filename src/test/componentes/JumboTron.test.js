369513325314e15d6a64e131bde236b5c284fa01
rinel <rineliniguezsosa@gmail.com>
2023-04-19 14:35:36 -0600

    creando el bloque test con el titulo:se muestra el titulo

--- src/test/componentes/JumboTron.test.js
+++ src/test/componentes/JumboTron.test.js
@@ -2,5 +2,7 @@ import { render,screen } from '@testing-library/react'
 import { JumboTron } from '../../componentes/JumboTron'
 
 describe('pruebas en JumboTron.js', () => {
-    
+    test('se muestra el titulo', () => {
+        
+    });
 });
