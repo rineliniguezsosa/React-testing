09a42a699489f188d334496d8bbc51c745e45d06
rinel <rineliniguezsosa@gmail.com>
2023-04-19 14:48:02 -0600

    importamos jest-dom

--- src/test/componentes/JumboTron.test.js
+++ src/test/componentes/JumboTron.test.js
@@ -1,5 +1,6 @@
 import { render,screen } from '@testing-library/react'
 import { JumboTron } from '../../componentes/JumboTron'
+import '@testing-library/jest-dom/extend-expect'
 
 describe('pruebas en JumboTron.js', () => {
     test('se muestra el titulo', () => {
